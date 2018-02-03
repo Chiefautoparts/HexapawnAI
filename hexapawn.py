6002b5b229ab545cf1c18e41397d1b99d6f62844
Chiefautoparts <chiefautoparts@outlook.com>
2018-02-03 01:36:14 -0800

    zero errors

--- hexapawn.py
+++ hexapawn.py
@@ -4,9 +4,9 @@ class GameController(TwoPlayersGame):
     def __init__(self, players, size= (4,4)):
         self.size = size
         num_pawns, len_board = size
-        p = [[(i, j) for j in range(len_board)] for i in [0, num_oawns - 1]]
+        p = [[(i, j) for j in range(len_board)] for i in [0, num_pawns - 1]]
 
-        for i, d, goal, pawns in [(0, 1, num_pawns - 1, p[0], (1, -1, 0, p[1])]:
+        for i, d, goal, pawns in [(0, 1, num_pawns - 1, p[0]), (1, -1, 0, p[1])]:
             players[i].direction = d
             players[i].goal_line = goal
             players[i].pawns = pawns
@@ -15,7 +15,7 @@ class GameController(TwoPlayersGame):
         self.players = players
 
         # Define who starts first
-        self,nplayer = 1
+        self.nplayer = 1
 
         # Define the alphabets
         self.alphabets = 'ABCDEFGHIJ'
@@ -24,54 +24,54 @@ class GameController(TwoPlayersGame):
         self.to_tuple = lambda s: (self.alphabets.index(s[0]), int(s[1:]) - 1)
 
         # COnvert (1, 3) to B4
-        self.to_string = lambda move: ' '.join([self.alphabets[move[i][0]] + str(move[i][1] + 1) for i in (o, 1)])
+        self.to_string = lambda move: ' '.join([self.alphabets[move[i][0]] + str(move[i][1] + 1) for i in (0, 1)])
 
-        # Define the possible moves
-        def possible_moves(self):
-            moves = []
-            opponent_pawns = self.opponent.pawns
-            d = self.player.direction
+    # Define the possible moves
+    def possible_moves(self):
+        moves = []
+        opponent_pawns = self.opponent.pawns
+        d = self.player.direction
 
-            for i, j in self.player.pawns:
-                if (i + d, j) not in opponent_pawns:
-                    moves.append(((i, j), (i + d, j)))
+        for i, j in self.player.pawns:
+            if (i + d, j) not in opponent_pawns:
+                moves.append(((i, j), (i + d, j)))
 
-                if (i + d, j) in opponent_pawns:
-                    moves.append(((i, j), (i + d, j + 1)))
+            if (i + d, j) in opponent_pawns:
+                moves.append(((i, j), (i + d, j + 1)))
 
-            return list(map(self.to_string, [(i, j) for i, j in moves]))
+        return list(map(self.to_string, [(i, j) for i, j in moves]))
 
-        # Define how to make a move
-        def make_move(self, move):
-            move = list(map(self.to_tuple, move.slit(' ')))
-            ind = self.player.pawns.index(move[0])
-            self.player.pawns[ind] = move[i]
+    # Define how to make a move
+    def make_move(self, move):
+        move = list(map(self.to_tuple, move.split(' ')))
+        ind = self.player.pawns.index(move[0])
+        self.player.pawns[ind] = move[1]
 
-            if move[1] in self.opponent.pawns:
-                self.opponent.pawns.remove(move[1])
+        if move[1] in self.opponent.pawns:
+            self.opponent.pawns.remove(move[1])
 
-        # Define whtat a loss looks like
-        def loss_condition(self):
-            return (any([i == self.opponent.goal_line
-                    for i, j in self.opponent.pawns])
-                    or (self.possible_moves() == []) )
+    # Define whtat a loss looks like
+    def loss_condition(self):
+        return (any([i == self.opponent.goal_line
+                for i, j in self.opponent.pawns])
+                or (self.possible_moves() == []) )
 
-        # Check if the gaem is over
-        def is_over(self):
-            return self.loss_condition()
+    # Check if the gaem is over
+    def is_over(self):
+        return self.loss_condition()
 
-        # Show the current status
-        def show(self):
-            f = lambda x: '1' if x in self.players[0].pawns else (
-                    '2' if x in self.players[1].pawns else '.')
+    # Show the current status
+    def show(self):
+        f = lambda x: '1' if x in self.players[0].pawns else (
+                '2' if x in self.players[1].pawns else '.')
 
-            print("\n",join([" ",join([f((i, j))
-                    for j in range(self.size[1])])
-                    for i in range(self.size[0])]))
+        print("\n".join([" ".join([f((i, j))
+                for j in range(self.size[1])])
+                for i in range(self.size[0])]))
 
-if _name__=='__main__':
+if __name__=='__main__':
     # Compute the score
-    scoring = lambda game: -100 if game.loss_conition() else 0
+    scoring = lambda game: -100 if game.loss_condition() else 0
 
     # Define the algorithm
     algorithm = Negamax(12, scoring)
